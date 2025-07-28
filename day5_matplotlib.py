2669079809ffc803ff0fb1c4e8049e0df2db54cc
Mahli <data.ilhamr@gmail.com>
2025-07-28 19:07:43 +0700

    add matplotlib

---
+++ day5_matplotlib.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0,11,1)
+y = 2*x + 3
+print(x)
+print(y)
+
+plt.plot(x,y)
+plt.show()
---
+++ day5_matplotlib_pendahuluan.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 1. membuat data
+# 2. membuat plot
+# 3. menampilkan plot
+
+x=np.array([1,2,3,4,5])
+y=np.array([1,4,9,16,25])
+
+plt.plot(x,y)
+
+plt.show()
